23759ae3c25d773270e982fec496b536a767f285
vadymmerezhko <vadymm@gmail.com>
2023-11-26 17:52:53 -0800

    Git call was added.

--- src/test/java/org/example/RunTest.java
+++ src/test/java/org/example/RunTest.java
@@ -7,11 +7,6 @@ public class RunTest {
     @Test
     public void RunLambdaTest() {
         TestRunner runner = new TestRunner();
-/*        runner.run(
-                //"C:\\Users\\vadym\\IdeaProjects\\GitHub\\SeleniumAWS",
-                "/home/ec2-user/test/SeleniumAWS",
-                "org.example.Selenium1Test",
-                "signUp1");*/
         runner.run("./ org.example.Selenium1Test signUp1");
     }
 }
