0394168aba7ed00166a9e18b7e218c5661d6dd64
vadymmerezhko <vadymm@gmail.com>
2023-11-26 18:11:02 -0800

    Shall file is fixed.

--- run-test.sh
+++ run-test.sh
@@ -1,3 +1,5 @@
+mkdir test
+cd test
 git pull https://github.com/vadymmerezhko/SeleniumAWS.git
 cd SeleniumAWS
 mvn test "-DtestSuite=./src/test/resources/org.example.Selenium1Test.signUp1.xml" "-DthreadCount=1" "-Dlambda=no"
