7d4a158022334193a942cfc9c0204f87cc009113
vadymmerezhko <vadymm@gmail.com>
2023-11-26 18:26:02 -0800

    Shall file is fixed.

--- run-test.sh
+++ run-test.sh
@@ -1,5 +1,6 @@
-mkdir test
+mkdir -p test
 cd test
+rm -r SeleniumAWS
 git clone https://github.com/vadymmerezhko/SeleniumAWS.git; echo "SeleniumAWS is cloned. "
 cd SeleniumAWS
 mvn test "-DtestSuite=./src/test/resources/org.example.Selenium1Test.signUp1.xml" "-DthreadCount=1" "-Dlambda=no"
