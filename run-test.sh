0db080d453373fdbdbb752fc2270448ae4daa9af
vadymmerezhko <vadymm@gmail.com>
2023-12-09 18:36:53 -0800

    Zip file names were fixed.

--- run-test.sh
+++ run-test.sh
@@ -3,10 +3,10 @@ cd SeleniumAWS
 sudo yum install unzip -y
 
 wget 	https://edgedl.me.gvt1.com/edgedl/chrome/chrome-for-testing/120.0.6099.71/linux64/chrome-linux64.zip
-unzip chrome-linux64
+unzip chrome-linux64.zip
 
 wget https://edgedl.me.gvt1.com/edgedl/chrome/chrome-for-testing/120.0.6099.71/linux64/chromedriver-linux64.zip
-unzip chromedriver-linux64
+unzip chromedriver-linux64.zip
 
 git clone https://github.com/vadymmerezhko/SeleniumAWS.git
 mvn test "-DtestSuite=src/test/resources/$1" "-DthreadCount=1" "-Dlambda=no"
