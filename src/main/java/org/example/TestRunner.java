4abefe227b0f844433c05b2f6af9061e04c5d6c3
vadymmerezhko <vadymm@gmail.com>
2023-12-12 13:25:51 -0800

    Run test command is fixed.

--- src/main/java/org/example/TestRunner.java
+++ src/main/java/org/example/TestRunner.java
@@ -49,7 +49,7 @@ public class TestRunner {
         //command = String.format("sh run-test.sh %s", fileName);
         //command = "cmd run-test.bat";
         command = String.format("""
-                cd "/tmp/SeleniumAWS";
+                cd "/tmp/SeleniumAWS"
                 mvn test "-DtestSuite=/tmp/SeleniumAWS/src/test/resources/%s" "-DthreadCount=1" "-Dlambda=no"
                 """,
                 fileName);
