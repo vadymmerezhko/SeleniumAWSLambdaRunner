149d251af89663642d5ca4be6212ddd6e25a326a
vadymmerezhko <vadymm@gmail.com>
2023-12-12 11:56:36 -0800

    TestNG file creation moved up.

--- run-test.sh
+++ run-test.sh
@@ -1,2 +1,2 @@
-cd SeleniumAWS || exit
+cd /tmp/SeleniumAWS || exit
 mvn test "-DtestSuite=src/test/resources/$1" "-DthreadCount=1" "-Dlambda=no"
--- src/main/java/org/example/TestRunner.java
+++ src/main/java/org/example/TestRunner.java
@@ -13,41 +13,43 @@ public class TestRunner {
         String[] params = data.split(" ");
         String className = params[0];
         String methodName = params[1];
-        String fileFolderPath = "SeleniumAWS/src/test/resources";
+        String fileFolderPath = "/tmp/SeleniumAWS/src/test/resources";
         String fileName = String.format("%s.%s.xml", className, methodName);
         String fileContent = Settings.TEST_NG_METHOD_FILE_TEMPLATE;
         fileContent = fileContent.replace(Settings.CLASS_NAME_PLACEHOLDER, className);
         fileContent = fileContent.replace(Settings.METHOD_NAME_PLACEHOLDER, methodName);
 
+        String command;
+        String output;
+
         GitManager.cloneRepository(
                 "https://github.com/vadymmerezhko/SeleniumAWS.git",
-                "SeleniumAWS");
+                "/tmp/SeleniumAWS");
         System.out.println("Git repository is cloned");
-
-        WebFileDownloadManager.download(
-                "https://edgedl.me.gvt1.com/edgedl/chrome/chrome-for-testing/120.0.6099.71/linux64/chrome-linux64.zip",
-                "chrome-linux64.zip");
-        System.out.println("Chrome browser ZIP is downloaded");
-
-        WebFileDownloadManager.download(
-                "https://edgedl.me.gvt1.com/edgedl/chrome/chrome-for-testing/120.0.6099.71/linux64/chromedriver-linux64.zip",
-                "chromedriver-linux64.zip");
-        System.out.println("Chrome driver ZIP is downloaded");
-
-        ZipManager.unzip("chrome-linux64.zip", "SeleniumAWS/chrome-linux64");
-        System.out.println("Chrome browser ZIP is unzipped");
-
-        ZipManager.unzip("chromedriver-linux64.zip", "SeleniumAWS/chromedriver-linux64");
-        System.out.println("Chrome driver ZIP is unzipped");
 
         FileManager.createFile(fileFolderPath, fileName, fileContent);
         System.out.println("TestNG file is created");
 
-        String command = String.format("sh run-test.sh %s", fileName);
+        WebFileDownloadManager.download(
+                "https://edgedl.me.gvt1.com/edgedl/chrome/chrome-for-testing/120.0.6099.71/linux64/chrome-linux64.zip",
+                "/tmp/chrome-linux64.zip");
+        System.out.println("Chrome browser ZIP is downloaded");
+
+        WebFileDownloadManager.download(
+                "https://edgedl.me.gvt1.com/edgedl/chrome/chrome-for-testing/120.0.6099.71/linux64/chromedriver-linux64.zip",
+                "/tmp/chromedriver-linux64.zip");
+        System.out.println("Chrome driver ZIP is downloaded");
+
+        ZipManager.unzip("/tmp/chrome-linux64.zip", "/tmp/chrome-linux64");
+        System.out.println("Chrome browser ZIP is unzipped");
+
+        ZipManager.unzip("/tmp/chromedriver-linux64.zip", "/tmp/chromedriver-linux64");
+        System.out.println("Chrome driver ZIP is unzipped");
+
+        command = String.format("sh run-test.sh %s", fileName);
         //command = "cmd run-test.bat";
         System.out.println("Run shell file: " + command);
-
-        String output = CommandLineExecutor.runCommandLine(command);
+        output = CommandLineExecutor.runCommandLine(command);
         System.out.println("Output:\n" + output);
 
         if (!output.contains("BUILD SUCCESS")) {
